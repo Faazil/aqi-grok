76c22dea114d4d2b048c4dc94391bdd8195d1187
Faazil-uddin-007 <fazil434@gmail.com>
2025-12-14 03:09:56 +0530

    Update AqiMap.tsx

--- app/components/AqiMap.tsx
+++ app/components/AqiMap.tsx
@@ -2,7 +2,6 @@
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
-import { useEffect } from 'react';
 
 interface CityData {
   name: string;
@@ -26,20 +25,6 @@ function getAqiColor(aqi: number) {
 }
 
 export default function AqiMap({ cityData }: AqiMapProps) {
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const L = require('leaflet');
-      delete (L.Icon.Default.prototype as any)._getIconUrl;
-      L.Icon.Default.mergeOptions({
-        iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
-        iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
-      });
-    }
-  }, []);
-
-  if (typeof window === 'undefined') return null;
-
   return (
     <MapContainer center={[20.5937, 78.9629]} zoom={5} style={{ height: '100%', width: '100%' }}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
