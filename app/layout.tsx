02338699165441ba62ebd69786d9056a0b4b6a8a
Faazil-uddin-007 <fazil434@gmail.com>
2025-12-16 02:09:12 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import "./globals.css";
 import "leaflet/dist/leaflet.css";
+import { ReactNode } from "react";
 
 export const metadata = {
   title: "AQI India",
   description: "Live AQI across Indian cities",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
