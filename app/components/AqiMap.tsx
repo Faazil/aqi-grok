753a2b6db4941534dcd03c75357b7a080b5e56d6
Faazil-uddin-007 <fazil434@gmail.com>
2025-12-15 14:19:12 +0530

    Update AqiMap.tsx

--- app/components/AqiMap.tsx
+++ app/components/AqiMap.tsx
@@ -2,6 +2,7 @@
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
+import { useEffect } from 'react';
 
 export interface CityData {
   name: string;
@@ -20,32 +21,42 @@ const icon = new L.Icon({
 
 export default function AqiMap({ cityData }: { cityData: CityData[] }) {
   return (
-    <MapContainer
-      center={[22.5937, 78.9629]}
-      zoom={5}
-      scrollWheelZoom={false}
-      style={{ height: '520px', width: '100%' }}
+    <div
+      style={{
+        width: '100%',
+        height: '520px',
+        borderRadius: '16px',
+        overflow: 'hidden',
+        background: '#111',
+      }}
     >
-      <TileLayer
-        attribution="&copy; OpenStreetMap contributors"
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <MapContainer
+        center={[22.5937, 78.9629]}
+        zoom={5}
+        scrollWheelZoom={false}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer
+          attribution="© OpenStreetMap contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
 
-      {cityData.map((city) => (
-        <Marker
-          key={city.name}
-          position={[city.lat, city.lng]}
-          icon={icon}
-        >
-          <Popup>
-            <strong>{city.name}</strong>
-            <br />
-            AQI: {city.aqi}
-            <br />
-            {city.level}
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+        {cityData.map((city) => (
+          <Marker
+            key={city.name}
+            position={[city.lat, city.lng]}
+            icon={icon}
+          >
+            <Popup>
+              <strong>{city.name}</strong>
+              <br />
+              AQI: {city.aqi}
+              <br />
+              {city.level}
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
   );
 }
